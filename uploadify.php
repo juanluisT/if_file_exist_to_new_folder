9a6f683fa9380a2aab40c7ca0f74631bed025411
Juan <juanluisT@users.noreply.github.com>
2014-09-18 16:09:16 -0700

    Update uploadify.php

--- uploadify.php
+++ uploadify.php
@@ -6,12 +6,6 @@ echo $archivo = $_FILES['Filedata'];
 	echo  "exists";
 } else {*/
 // Define adestination
-    $bd_host = "localhost";
-	$bd_usuario = "root";
-	$bd_password = "root";
-	$bd_base = "calidad";
-	$con = mysql_connect($bd_host, $bd_usuario, $bd_password) or die("Error en la conexión a MsSql");
-	mysql_select_db($bd_base, $con);
 	$archivo = $_FILES['Filedata']['name'];
 	/*
 	$sql = "select * from photos where code='$archivo'";
@@ -20,18 +14,18 @@ echo $archivo = $_FILES['Filedata'];
 	if($num_rs_per == 0) { 
 	*/
      $targetFolder = '/uploads'; // Relative to the root
-     $targetPath = $_SERVER['DOCUMENT_ROOT'] ."/administrator/gestion_samples/photodrop". $targetFolder;
+     $targetPath = $_SERVER['DOCUMENT_ROOT'] ."/". $targetFolder;
     $verifyToken = md5('unique_salt' . $_POST['timestamp']);
 if (!empty($_FILES) && $_POST['token'] == $verifyToken) {
 	$tempFile = $_FILES['Filedata']['tmp_name'];
-	 $targetPath = $_SERVER['DOCUMENT_ROOT'] ."/administrator/gestion_samples/photodrop". $targetFolder;
+	 $targetPath = $_SERVER['DOCUMENT_ROOT'] ."/". $targetFolder;
 	$targetFile = rtrim($targetPath,'/') . '/' . $_FILES['Filedata']['name'];
 	// Validate the file type
 	$fileTypes = array('jpg','jpeg','gif','png','xls','xlsx'); // File extensions
 	$fileParts = pathinfo($_FILES['Filedata']['name']);
 // insert into DB	
 	$archivo=$_FILES['Filedata']['name'];
-	$con=mysqli_connect("localhost","root","root","calidad");
+	$con=mysqli_connect("localhost","root","root","DB_name");
 // Check connection
 if (mysqli_connect_errno())
   {
